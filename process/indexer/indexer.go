bde559489b998e32df712e04397789e79f8e3028
Marius C <marius.ciucam@gmail.com>
2023-03-22 17:00:59 +0200

    FIX: After self review

--- process/indexer/indexer.go
+++ process/indexer/indexer.go
@@ -9,6 +9,8 @@ type indexer struct {
 	notifier process.SovereignNotifier
 }
 
+// NewIndexer creates an indexer which will internally save received blocks
+// and notify sovereign shards for each finalized block
 func NewIndexer(notifier process.SovereignNotifier) (process.Indexer, error) {
 	if notifier == nil {
 		return nil, errNilSovereignNotifier
@@ -19,10 +21,13 @@ func NewIndexer(notifier process.SovereignNotifier) (process.Indexer, error) {
 	}, nil
 }
 
+// SaveBlock will save the received block in an internal cache
 func (i *indexer) SaveBlock(_ *outport.OutportBlock) error {
 	return nil
 }
 
+// FinalizedBlock will check the finalized header for incoming txs
+// to sovereign shard and push the finalized block through notifier
 func (i *indexer) FinalizedBlock(_ *outport.FinalizedBlock) error {
 	return nil
 }
